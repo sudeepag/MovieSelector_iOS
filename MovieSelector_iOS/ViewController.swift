7a40d74f1b48c885ed7849b7e2e374e055522518
Sudeep Agarwal <sudeep135@gmail.com>
2016-03-08 17:25:44 -0500

    Initial commit.

---
+++ MovieSelector_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieSelector_iOS
+//
+//  Created by Sudeep Agarwal on 3/8/16.
+//  Copyright © 2016 Sudeep Agarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'MovieSelector_iOS' do
+
+end
+
+target 'MovieSelector_iOSTests' do
+
+end
+
+target 'MovieSelector_iOSUITests' do
+
+end
+
